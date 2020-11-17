2ee7dc35e9cd5528e8ca44c53b5da018516d8f6d
Greg Seiler <ashimar229@gmail.com>
2020-11-17 12:30:53 -0500

    Finished site

--- scripts/script.js
+++ scripts/script.js
@@ -1,108 +1,82 @@
 // variable for the display element
 let displayElem = document.getElementById('calc-display');
-let nums = [0,0];
-// variable for keeping track of the current operation so it can send the last number inputed to the proper array after hitting equals
-let currentOp = '';
 // boolean for resetting display after the operation has been selected
-let resetDisplay = false;
+let totaled = false;
+let isSymbol = false;
 
-let numCount = 0;
 
-
-// function to clear the arrays in the problem class
-function clearProblem(){
-    nums = [0,0];
-    numCount = 0;
-    currentOp = '';
-}
-
+//function to clear the display
 function clearAll(){
-    clearProblem();
     displayElem.value = "";
 }
 
-// function to take input from form and add it to the display and reset it if an operation has been selected
+// function to take input from form and add it to the display and reset it if equals has been selected
 function num(val){
-    if(resetDisplay){
-        displayElem.value = "";
-        resetDisplay = false;
+    isSymbol = /\+|\-|\*|\//.test(val);
+    if(totaled && !isSymbol){
+        clearAll();
     }
-    if(val == "."){
-        if(displayElem.value.includes(".")){
-            val = '';
+    totaled = false;
+    displayElem.value = displayElem.value + val;
+    if(!isSymbol){
+        flash(val); 
+    }
+    document.getElementById(val).blur();  
+}
+
+
+// function to solve equation in the display
+function solve(){
+    displayElem.value = calculate(displayElem.value);
+    totaled = true;
+}
+
+
+//resets the key to the default button of bootsrap
+function resetKey(theKey){
+    document.getElementById(theKey).classList.add('btn-default');
+    document.getElementById(theKey).classList.remove('btn-primary');
+}
+
+
+//makes the button typed flash to denote it has been pressed
+function flash(theKey){
+    document.getElementById(theKey).classList.remove('btn-default');
+    document.getElementById(theKey).classList.add('btn-primary');
+    setTimeout(function (){resetKey(theKey)}, 100);
+}
+
+//calculates the equation in the display
+function calculate(fn){
+    return new Function('return ' + fn)();
+}
+
+
+//function to handle key input
+function handleKeys(e){
+    //if the key pressed is a number or period it is sent to the num function, if the key is a space then it is ignored
+    if(e.key != ' '){
+        if(e.key >= 0 && e.key <= 9){
+            num(e.key);
+        }else if(e.key === '.'){
+            num(e.key);
+        }else if(/\+|\-|\*|\//.test(e.key)){  //if the key pressed is a math symbol then it is sent to the num function
+            num(e.key);
+        }
+        // handled the equals/enter key and the c/ac key
+        switch(e.key){
+            case "Enter":
+               solve(); 
+               break;
+            case "c":
+                clearAll();
+                break;
+            default:
+                break;
         }
     }
-    displayElem.value = displayElem.value + val;    
-}
-
-
-// function to add to numbers to the appropriate array in the problem class
-function oper(op){
-    nums[numCount] = displayElem.value;
-    numCount = (numCount == 1) ? 0 : 1;
-    resetDisplay = true;
-    if(numCount == 0){
-        solve(false);
-    }else{
-        currentOp = op;
-    }
-}
-
-function solve(fromEquals){
-    if(fromEquals){
-        oper(currentOp);
-    }
-      // send this to the appropriate object array
-    console.log(nums)
-    switch(currentOp){
-        case "addition":
-            displayElem.value = parseFloat(nums[0]) + parseFloat(nums[1]);
-            break;   
-        case "subtraction":
-            displayElem.value = parseFloat(nums[0]) - parseFloat(nums[1]);
-            break;
-        case "multiplication":
-            displayElem.value = parseFloat(nums[0]) * parseFloat(nums[1]);
-            break;
-        case "division":
-            displayElem.value = parseFloat(nums[0]) / parseFloat(nums[1]);
-            break;
-        default:
-            break;
-
-    }
-    clearProblem();  
     
 }
-function handleKeys(e){
-    if(e.key >= 0 && e.key <= 9){
-        num(e.key);
-    }else if(e.key === '.'){
-        num(e.key);
-    }
-    switch(e.key){
-        case "+":
-            oper("addition");
-            break; 
-        case "-":
-            oper("subtraction");
-            break; 
-        case "*":
-            oper("multiplication");
-            break;
-        case "/":
-            oper("division");
-            break; 
-        case "Enter":
-           solve(true); 
-           break;
-        case "Space":
-            clearAll();
-            break;
-        default:
-            break;
-    }
-    console.log(e.key)
-    
-}
+
+//event listener for entire page to check for key presses
 document.addEventListener('keydown', handleKeys)
